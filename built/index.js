e6011ad1c4739070687c5bea6583433110f55846
Willian K <wkrueger128@gmail.com>
2018-04-13 17:42:21 -0300

    when i extracted config into a file I didnt test it

--- built/index.js
+++ built/index.js
@@ -5,22 +5,22 @@ const config = require("./config");
 (async () => {
     function clickNWait(selector) {
         return Promise.all([
-            page.waitForNavigation({ waitUntil: 'networkidle2' }),
+            page.waitForNavigation({ waitUntil: "networkidle2" }),
             page.click(selector)
         ]);
     }
     const browser = await puppet.launch({ headless: false, slowMo: 200 });
     const page = await browser.newPage();
-    await page.goto('https://www.facebook.com');
-    await page.waitForSelector('input#email');
-    await page.$eval('input#email', el => el.value = config.username);
-    await page.$eval('input#pass', el => el.value = config.password);
-    await clickNWait('input[data-testid=royal_login_button]');
+    await page.goto("https://www.facebook.com");
+    await page.waitForSelector("input#email");
+    await page.$eval("input#email", (el, config) => (el.value = config.username), config);
+    await page.$eval("input#pass", (el, config) => (el.value = config.password), config);
+    await clickNWait("input[data-testid=royal_login_button]");
     await page.waitFor(3000);
     await page.goto(`https://www.facebook.com/${config.profile}`);
     let skip = config.skip || [];
     while (true) {
-        let current = '';
+        let current = "";
         try {
             let storyOpt = 'a[aria-label="Story options"]';
             await page.waitForSelector(storyOpt);
@@ -28,7 +28,7 @@ const config = require("./config");
                 window.scrollTo(0, 4000);
             });
             await page.waitFor(1000);
-            let ids = await page.$$eval('div[role=article]', divs => {
+            let ids = await page.$$eval("div[role=article]", divs => {
                 return Array.from(divs).map(div => div.id);
             });
             //let storyOpt = 'a[aria-label="Story options"]'
@@ -38,30 +38,30 @@ const config = require("./config");
                     continue;
                 if (skip.indexOf(id) !== -1)
                     continue;
-                if (String(id).startsWith('comment'))
+                if (String(id).startsWith("comment"))
                     continue;
-                let handle = await page.waitForSelector('#' + id + ' ' + storyOpt);
+                let handle = await page.waitForSelector("#" + id + " " + storyOpt);
                 await handle.click();
-                let deleteOpt = 'li[data-feed-option-name=FeedDeleteOption] > a';
+                let deleteOpt = "li[data-feed-option-name=FeedDeleteOption] > a";
                 await page.waitFor(1000);
                 try {
                     let delete1Handle = await page.$$(deleteOpt);
                     await delete1Handle[delete1Handle.length - 1].click();
                 }
                 catch (err) {
-                    let hideOpt = 'li[data-feed-option-name=HIDE_FROM_TIMELINE] > a';
+                    let hideOpt = "li[data-feed-option-name=HIDE_FROM_TIMELINE] > a";
                     let hideHandle = await page.$$(hideOpt);
                     await hideHandle[hideHandle.length - 1].click();
                 }
                 //await page.click(handle)
-                let deleteBtn = 'div.uiOverlayFooter button[type=submit]';
+                let deleteBtn = "div.uiOverlayFooter button[type=submit]";
                 let delete2Handle = await page.waitForSelector(deleteBtn);
                 await delete2Handle.click();
                 await page.waitFor(2000);
             }
         }
         catch (err) {
-            console.log('add to skip list ' + current);
+            console.log("add to skip list " + current);
             skip.push(current);
             page.goto(`https://www.facebook.com/${config.profile}`);
             await page.waitFor(2000);
--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,79 @@
-import * as puppet from 'puppeteer'
-import * as config from './config'
+import * as puppet from "puppeteer"
+import * as config from "./config"
+;(async () => {
+  function clickNWait(selector: string) {
+    return Promise.all([
+      page.waitForNavigation({ waitUntil: "networkidle2" }),
+      page.click(selector)
+    ])
+  }
 
-( async () => {
-    function clickNWait(selector: string) {
-        return Promise.all([
-            page.waitForNavigation({ waitUntil: 'networkidle2' }),
-            page.click(selector)
-        ])
-    }
+  const browser = await puppet.launch({ headless: false, slowMo: 200 })
+  const page = await browser.newPage()
+  await page.goto("https://www.facebook.com")
+  await page.waitForSelector("input#email")
+  await page.$eval(
+    "input#email",
+    (el, config) => ((el as HTMLInputElement).value = config.username),
+    config
+  )
+  await page.$eval(
+    "input#pass",
+    (el, config) => ((el as HTMLInputElement).value = config.password),
+    config
+  )
+  await clickNWait("input[data-testid=royal_login_button]")
+  await page.waitFor(3000)
+  await page.goto(`https://www.facebook.com/${config.profile}`)
 
-    const browser = await puppet.launch({ headless: false, slowMo: 200 })
-    const page = await browser.newPage()
-    await page.goto('https://www.facebook.com')
-    await page.waitForSelector('input#email')
-    await page.$eval('input#email', el => (el as HTMLInputElement).value = config.username )
-    await page.$eval('input#pass', el => (el as HTMLInputElement).value = config.password )
-    await clickNWait('input[data-testid=royal_login_button]')
-    await page.waitFor(3000)
-    await page.goto(`https://www.facebook.com/${config.profile}`)
+  let skip = config.skip || []
 
-    let skip = config.skip || []
+  while (true) {
+    let current = ""
+    try {
+      let storyOpt = 'a[aria-label="Story options"]'
+      await page.waitForSelector(storyOpt)
 
-    while (true) {
-        let current = ''
+      await page.evaluate(() => {
+        window.scrollTo(0, 4000)
+      })
+
+      await page.waitFor(1000)
+
+      let ids = await page.$$eval("div[role=article]", divs => {
+        return Array.from(divs).map(div => div.id)
+      })
+
+      //let storyOpt = 'a[aria-label="Story options"]'
+      for (let id of ids) {
+        current = id
+        if (!id) continue
+        if (skip.indexOf(id) !== -1) continue
+        if (String(id).startsWith("comment")) continue
+        let handle = await page.waitForSelector("#" + id + " " + storyOpt)
+        await handle.click()
+
+        let deleteOpt = "li[data-feed-option-name=FeedDeleteOption] > a"
+        await page.waitFor(1000)
         try {
-            let storyOpt = 'a[aria-label="Story options"]'
-            await page.waitForSelector(storyOpt)
-
-            await page.evaluate(() => {
-                window.scrollTo(0, 4000)
-            })
-
-            await page.waitFor(1000)
-
-            let ids = await page.$$eval('div[role=article]', divs => {
-                return Array.from(divs).map( div => div.id )
-            })
-
-            //let storyOpt = 'a[aria-label="Story options"]'
-            for (let id of ids) {
-                current = id
-                if (!id) continue
-                if (skip.indexOf(id) !== -1) continue
-                if (String(id).startsWith('comment')) continue
-                let handle = await page.waitForSelector('#' + id + ' ' + storyOpt)
-                await handle.click()
-
-                let deleteOpt = 'li[data-feed-option-name=FeedDeleteOption] > a'
-                await page.waitFor(1000)
-                try {
-                    let delete1Handle = await page.$$(deleteOpt)
-                    await delete1Handle[delete1Handle.length-1].click()
-                } catch(err) {
-                    let hideOpt = 'li[data-feed-option-name=HIDE_FROM_TIMELINE] > a'
-                    let hideHandle = await page.$$(hideOpt)
-                    await hideHandle[hideHandle.length-1].click()
-                }
-                //await page.click(handle)
-                let deleteBtn = 'div.uiOverlayFooter button[type=submit]'
-                let delete2Handle = await page.waitForSelector(deleteBtn)
-                await delete2Handle.click()
-                await page.waitFor(2000)                
-            }
+          let delete1Handle = await page.$$(deleteOpt)
+          await delete1Handle[delete1Handle.length - 1].click()
         } catch (err) {
-            console.log('add to skip list ' + current)
-            skip.push(current)
-            page.goto(`https://www.facebook.com/${config.profile}`)
-            await page.waitFor(2000)
+          let hideOpt = "li[data-feed-option-name=HIDE_FROM_TIMELINE] > a"
+          let hideHandle = await page.$$(hideOpt)
+          await hideHandle[hideHandle.length - 1].click()
         }
+        //await page.click(handle)
+        let deleteBtn = "div.uiOverlayFooter button[type=submit]"
+        let delete2Handle = await page.waitForSelector(deleteBtn)
+        await delete2Handle.click()
+        await page.waitFor(2000)
+      }
+    } catch (err) {
+      console.log("add to skip list " + current)
+      skip.push(current)
+      page.goto(`https://www.facebook.com/${config.profile}`)
+      await page.waitFor(2000)
     }
-} )()
+  }
+})()
